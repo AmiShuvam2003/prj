0bd9212f925043cec403c357ef69324427d873c7
Shuvam Samanta <161609142+AmiShuvam2003@users.noreply.github.com>
2025-06-16 20:26:11 +0530

    changed

--- read_array.py
+++ read_array.py
@@ -8,16 +8,16 @@ ctypes.CDLL(r"C:\msys64\ucrt64\bin\libstdc++-6.dll")
 lib = ctypes.windll.LoadLibrary(r"H:\Work\checkcodes\prj\array_lib.dll")  # Use 'array_lib.dll' on Windows
 
 # Set the return type to a pointer to an 8x8 double array
-lib.getarr.restype = ctypes.POINTER(ctypes.c_double * 8 * 8)
+lib.getarr.restype = ctypes.POINTER(ctypes.c_int * 16 * 1)
 
 # Get the array from C++
 ptr = lib.getarr()
 
 # Convert to a NumPy array for easier visualization
 matrix = np.ctypeslib.as_array(ptr, shape=(8, 8))
-
+matrix2= np.abs(matrix[:,:])
 
 # Print the matrix with 2 decimal places
-np.set_printoptions(precision=2, suppress=True)  # Suppress scientific notation
+#np.set_printoptions(round=matrix)  # Suppress scientific notation
 print("Python Retrieved Matrix:")
-print(matrix)
+print(matrix2)
